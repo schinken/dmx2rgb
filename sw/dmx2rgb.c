52ef46d61ec986f2b2293b819020812f06802531
schinken <schinken@hackerspace-bamberg.de>
2012-10-11 01:21:52 +0200

    dmx reading approach

--- sw/dmx2rgb.c
+++ sw/dmx2rgb.c
@@ -1,12 +1,17 @@
 #include <avr/io.h>
 #include <avr/interrupt.h>
 
+#define DMX_BAUD 250000
+
 #define DMX_BUFFERS         2
 #define DMX_BUFFER_1        0
 #define DMX_BUFFER_2        1
 
 #define DMX_CHANNEL         1   // DMX Channel
-#define DMX_NUM_CHANNELS   48   // How many channels? 
+#define DMX_NUM_CHANNELS    2   // How many channels? 
+
+#define F_CPU 16000000
+
 
 uint8_t dmx_rx_complete = 0x00;
 
@@ -27,69 +32,91 @@ inline void dmx_switch_buffers() {
     }
 }
 
+uint8_t  dmx_valid = 0;
 uint16_t rx_cnt;    // 16-bit for 512 channels
-uint8_t  dummy;     // used for catching dummy bytes
 
-ISR( USART_RX_vect , ISR_BLOCK ){
+ISR( USART_RX_vect ){
 
+    uint8_t tmp = UDR0;
+
+    PORTB ^= 0x01;
 
     // Check if stopbit is set - new package begins
-    if ( UCSR0A && (1 << FE0) ) {
+    if ( UCSR0A & (1 << FE0) ) {
  
-        // dummy read to clear frame error
-        while ( UCSR0A && (1 << RXC0) ) {
-            dummy = UDR0;
-        }
-
         rx_cnt = 0;
         dmx_rx_complete = 0;
 
-        dmx_switch_buffers();
+        dmx_buf_back[0] = tmp;
 
-    }else{
+		if(tmp == 0) {
+			dmx_valid = 1;
+            PORTB |= 0x02;
+            rx_cnt++;
+		}else{
+			dmx_valid = 0;
+            PORTB &= ~(0x02);
+		}
 
+
+//        dmx_switch_buffers();
+        return;
+    }
+
+    if( dmx_valid ) {
+
+        PORTB ^= 0x04;
         // Check if we are in range
         if( rx_cnt >= DMX_CHANNEL && rx_cnt <= (DMX_CHANNEL + DMX_NUM_CHANNELS) ){
 
-            dmx_buf_back[ rx_cnt - DMX_CHANNEL ] = UDR0;
+            dmx_buf_back[ rx_cnt - DMX_CHANNEL ] = tmp;
+        }
 
-            // If we've reached all channels we need, mark current package as completed
-            if( rx_cnt == (DMX_CHANNEL + DMX_NUM_CHANNELS) - 1 ) {
-                dmx_rx_complete = 1;
-            }
-
-        } else {
-            dummy = UDR0; //catch byte
+        // If we've reached all channels we need, mark current package as completed
+        if( rx_cnt == (DMX_CHANNEL + DMX_NUM_CHANNELS)-2 ) {
+            dmx_rx_complete = 1;
         }
 
         rx_cnt++;
     }
+
 }
- 
+
 int main (void) {            
 
 
     // INIT DMX
 
-    UCSR0C = (1<<USBS0) | (1<<UCSZ01) | (1<<UCSZ00); // 2 stop bits, 8 data bits
+
+    //UCSR0C = (1<<USBS0) | (1<<UCSZ01) | (1<<UCSZ00); // 2 stop bits, 8 data bits
+    UCSR0C = (1<<USBS0); // 2 stop bits, 8 data bits
     UCSR0B = (1<<RXEN0) | (1<<RXCIE0);               // enable transmit, receive and interrupts,
-    UBRR0H = 0;
-    UBRR0L = 3; //16 MHz->250kbps
+    UBRR0  = (F_CPU / (DMX_BAUD * 16L) - 1);
+ //   UBRR0H = 0;
+ //   UBRR0L = 3; //16 MHz->250kbps
  
     // INIT PORT PINS
     DDRB  = (1 << PIN0) | (1 << PIN1 ) | (1 << PIN2);
     PORTB = (1 << PIN0) | (1 << PIN1 ) | (1 << PIN2);
+
+//    DDRD = 0x02;
     
+    sei();
+
     unsigned int cnt = 0x00;
 
     while(1) {  
 
         if( dmx_rx_complete ) {
             PORTB ^= 0x02;
+
+            if( dmx_buf_front[1] == 42 ) {
+                PORTB ^= 0x04;    
+            }
         }
 
-        if( cnt == 0x00 ) {
-            PORTB ^= 0x01;
+        if( cnt == 0 ) {
+ //           PORTB ^= 0x04;    
         }
 
         cnt++;
