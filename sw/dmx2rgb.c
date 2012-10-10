79fc21c3a107c6995aba730bd6b567e3aef82b9b
schinken <schinken@hackerspace-bamberg.de>
2012-10-11 01:43:42 +0200

    working dmx rx

--- sw/dmx2rgb.c
+++ sw/dmx2rgb.c
@@ -8,12 +8,12 @@
 #define DMX_BUFFER_2        1
 
 #define DMX_CHANNEL         1   // DMX Channel
-#define DMX_NUM_CHANNELS    2   // How many channels? 
+#define DMX_NUM_CHANNELS    10   // How many channels? 
 
 #define F_CPU 16000000
 
 
-uint8_t dmx_rx_complete = 0x00;
+volatile uint8_t dmx_rx_complete = 0x00;
 
 uint8_t dmx_buffers[ DMX_BUFFERS ][ DMX_NUM_CHANNELS + 2 ];
 
@@ -33,29 +33,25 @@ inline void dmx_switch_buffers() {
 }
 
 uint8_t  dmx_valid = 0;
-uint16_t rx_cnt;    // 16-bit for 512 channels
+uint16_t dmx_rx_cnt;    // 16-bit for 512 channels
 
 ISR( USART_RX_vect ){
 
     uint8_t tmp = UDR0;
 
-    PORTB ^= 0x01;
-
     // Check if stopbit is set - new package begins
     if ( UCSR0A & (1 << FE0) ) {
  
-        rx_cnt = 0;
+        dmx_rx_cnt = 0;
         dmx_rx_complete = 0;
 
         dmx_buf_back[0] = tmp;
 
 		if(tmp == 0) {
 			dmx_valid = 1;
-            PORTB |= 0x02;
-            rx_cnt++;
+            dmx_rx_cnt++;
 		}else{
 			dmx_valid = 0;
-            PORTB &= ~(0x02);
 		}
 
 
@@ -65,19 +61,19 @@ ISR( USART_RX_vect ){
 
     if( dmx_valid ) {
 
-        PORTB ^= 0x04;
         // Check if we are in range
-        if( rx_cnt >= DMX_CHANNEL && rx_cnt <= (DMX_CHANNEL + DMX_NUM_CHANNELS) ){
+        if( dmx_rx_cnt >= DMX_CHANNEL && dmx_rx_cnt <= (DMX_CHANNEL + DMX_NUM_CHANNELS + 1 ) ){
 
-            dmx_buf_back[ rx_cnt - DMX_CHANNEL ] = tmp;
+            dmx_buf_back[ dmx_rx_cnt - DMX_CHANNEL +1 ] = tmp;
+
+            // If we've reached all channels we need, mark current package as completed
+            if( dmx_rx_cnt == (DMX_CHANNEL + DMX_NUM_CHANNELS) ) {
+                dmx_rx_complete = 1;
+            }
         }
 
-        // If we've reached all channels we need, mark current package as completed
-        if( rx_cnt == (DMX_CHANNEL + DMX_NUM_CHANNELS)-2 ) {
-            dmx_rx_complete = 1;
-        }
 
-        rx_cnt++;
+        dmx_rx_cnt++;
     }
 
 }
@@ -86,40 +82,38 @@ int main (void) {
 
 
     // INIT DMX
-
-
-    //UCSR0C = (1<<USBS0) | (1<<UCSZ01) | (1<<UCSZ00); // 2 stop bits, 8 data bits
-    UCSR0C = (1<<USBS0); // 2 stop bits, 8 data bits
+    UCSR0C = (1<<USBS0) | (1<<UCSZ01) | (1<<UCSZ00); // 2 stop bits, 8 data bits
     UCSR0B = (1<<RXEN0) | (1<<RXCIE0);               // enable transmit, receive and interrupts,
     UBRR0  = (F_CPU / (DMX_BAUD * 16L) - 1);
- //   UBRR0H = 0;
- //   UBRR0L = 3; //16 MHz->250kbps
  
     // INIT PORT PINS
     DDRB  = (1 << PIN0) | (1 << PIN1 ) | (1 << PIN2);
     PORTB = (1 << PIN0) | (1 << PIN1 ) | (1 << PIN2);
 
-//    DDRD = 0x02;
-    
     sei();
-
-    unsigned int cnt = 0x00;
 
     while(1) {  
 
         if( dmx_rx_complete ) {
-            PORTB ^= 0x02;
 
-            if( dmx_buf_front[1] == 42 ) {
-                PORTB ^= 0x04;    
+            if( dmx_buf_back[1] == 42 ) {
+                PORTB &= ~0x01;    
+            } else {
+                PORTB |= 0x01;
+            }
+
+            if( dmx_buf_back[2] == 42 ) {
+                PORTB &= ~0x02;    
+            } else {
+                PORTB |= 0x02;
+            }
+
+            if( dmx_buf_back[3] == 42 ) {
+                PORTB &= ~0x04;    
+            } else {
+                PORTB |= 0x04;
             }
         }
-
-        if( cnt == 0 ) {
- //           PORTB ^= 0x04;    
-        }
-
-        cnt++;
     }                         
  
     return 0;                 
