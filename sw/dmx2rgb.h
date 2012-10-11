2199494411dad0ac856bff001074aa2f8e1be893
schinken <schinken@hackerspace-bamberg.de>
2012-10-11 20:43:53 +0200

    Added configuration file

---
+++ sw/dmx2rgb.h
@@ -0,0 +1,18 @@
+
+#define DMX_BAUD 250000
+
+#define DMX_BUFFERS         2
+#define DMX_BUFFER_1        0
+#define DMX_BUFFER_2        1
+
+#define DMX_CHANNEL         1   // DMX Channel
+#define DMX_NUM_CHANNELS    10   // How many channels? 
+
+#define F_CPU 16000000
+
+#define PCA9685_CHIP_1 0b10000010
+#define PCA9685_CHIP_2 0b10000110
+#define PCA9685_CHIP_3 0b10001110
+
+#define LED_ENABLE()  PORTB &= ~(1 << PIN3)
+#define LED_DISABLE() PORTB |= (1 << PIN3)
